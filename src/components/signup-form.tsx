4702c6654a92938e3cf0a8c8f30fed6b4dd7e437
Diego Solano <solanolomasnolberto@gmail.com>
2026-01-30 15:27:30 +0000

    sigo sin poder ingresar, quizas si implementas la logica de  : const axi

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -56,7 +56,7 @@ export function SignupForm() {
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
-        const response = await fetch('https://api.solano.com/api/usuarios', {
+        const response = await fetch('https://api.solanoafk.com/api/usuarios', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
